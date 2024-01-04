e76b1b2c8ff28ae97c39f35d1c557a638d667ec0
mightykrauser <krauser1918@gmail.com>
2024-01-04 06:32:37 -0300

    header
    proyectohasta header

--- calculadoraapp/src/App.tsx
+++ calculadoraapp/src/App.tsx
@@ -1,34 +1,41 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
+
+import { Container,Header,WrapperSwitch,Switch,SwitcherContainer,Switcher } from './App.style'
+import GlobalStyle1 from './Styles/theme1'
+import GlobalStyle2 from './Styles/theme2'
+import GlobalStyle3 from './Styles/theme3'
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    {<GlobalStyle1 />}
+      <Container>
+        <Header>
+          Calc
+          <WrapperSwitch>
+            theme 
+            <Switch>
+              <div>
+                <span>1</span>
+                <span>2</span>
+                <span>3</span>
+              </div>
+              <SwitcherContainer>
+                <Switcher />
+
+                
+                </SwitcherContainer>  
+
+            </Switch>
+
+          </WrapperSwitch>
+        </Header>
+
+      </Container>
     </>
+      
   )
 }
 
